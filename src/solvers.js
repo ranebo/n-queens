458b2e4e3c7bf9a94e7a1ffd061746c68d969a3c
Rane Gridley <ranegri3@gmail.com>
2016-03-10 17:34:15 -0800

    Complete up to countNQueens

--- src/solvers.js
+++ src/solvers.js
@@ -49,71 +49,129 @@ window.findNRooksSolution = function(n) {
 
 
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+ // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var possibleSpots = n;
-  var fact = function(n) {
-    if (n === 0) {
-      return 1;
-    } else {
-      return n * fact(n - 1);
+  var solution = new Board({'n': n});
+  solutionCount = 0;
+  var helper = function(n, row) {
+
+    if (n - row === 1) {
+      //solution.togglePiece(row, i);
+      solutionCount++;
+      return;
+    }
+
+    for (var i = 0; i < n; i++) {
+      solution.togglePiece(row, i);
+      if (solution.hasAnyRooksConflicts()) {
+        solution.togglePiece(row, i);
+      } else {
+        helper(n, row + 1);
+        solution.togglePiece(row, i);
+      }
+
     }
   };
-  var solutionCount = fact(possibleSpots); //fixme
+  helper(n, 0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = new Board({'n': n});
-  var randIndexs = _.range(0, n);
-  var colNum = n;
 
- // var helper = function(n) {
-  //   if (n) {
-  //     var newRow = _.range(0, colNum).map(function(i) {
-  //       return 0;
-  //     });
-  //     solution.set(n - 1, newRow);
+  var board = new Board({'n': n});
+  var helper = function(n, row, board) {
+    if (row === n) {
+      return _.map(board.rows(), function(row) {
+        return row.slice();
+      });
+    }
 
-  //     var shuffRandIndexs = _.shuffle(randIndexs);
-  //     indexsUsed = indexsUsed || {};
-  //     indexsUsed[n - 1] = indexsUsed[n - 1] || [];
-  //     for (var i = 0; i < shuffRandIndexs.length; i++) {
-  //       if ( shuffRanIndexs)
-  //         solution.togglePiece(n - 1, shuffRandIndexs[i]);
-  //         if (!solution.hasAnyQueenConflictsOn(n - 1, shuffRandIndexs[i])) {
-  //           randIndexs.splice(randIndexs.indexOf(shuffRandIndexs[i]), 1);
-  //           indexsUsed[n - 1].push(shuffRandIndexs[i]);
-  //           helper(n - 1);
-  //           return;
-  //         } else {
-  //           solution.togglePiece(n - 1, shuffRandIndexs[i]);
-  //         }
-  //     }
+    for (var i = 0; i < n; i++) {
+      board.togglePiece(row,i);
+      if (!board.hasAnyQueensConflicts()) {
+        var result = helper(n, row + 1, board);
+        if (result) {
+          return result;
+        }
+      }
+      board.togglePiece(row, i);
+    }
+  };
+  var solution = helper(n, 0, board);
+  solution = solution || board.rows();
 
-  //     if (indexsUsed[n-1].length === colNum) {
-  //       helper(colNum, indexsUsed);
-  //     }
-  //     helper(n - 1);
-  //   }
-  //  };
-  // helper(n);
 
-  // console.log(solution.rows())
+
+ //  var solution = new Board({'n': n});
+ //  var randIndexs = _.range(0, n);
+ //  var colNum = n;
+
+
+ // // var helper = function(n) {
+ //  //   if (n) {
+ //  //     var newRow = _.range(0, colNum).map(function(i) {
+ //  //       return 0;
+ //  //     });
+ //  //     solution.set(n - 1, newRow);
+
+ //  //     var shuffRandIndexs = _.shuffle(randIndexs);
+ //  //     indexsUsed = indexsUsed || {};
+ //  //     indexsUsed[n - 1] = indexsUsed[n - 1] || [];
+ //  //     for (var i = 0; i < shuffRandIndexs.length; i++) {
+ //  //       if ( shuffRanIndexs)
+ //  //         solution.togglePiece(n - 1, shuffRandIndexs[i]);
+ //  //         if (!solution.hasAnyQueenConflictsOn(n - 1, shuffRandIndexs[i])) {
+ //  //           randIndexs.splice(randIndexs.indexOf(shuffRandIndexs[i]), 1);
+ //  //           indexsUsed[n - 1].push(shuffRandIndexs[i]);
+ //  //           helper(n - 1);
+ //  //           return;
+ //  //         } else {
+ //  //           solution.togglePiece(n - 1, shuffRandIndexs[i]);
+ //  //         }
+ //  //     }
+
+ //  //     if (indexsUsed[n-1].length === colNum) {
+ //  //       helper(colNum, indexsUsed);
+ //  //     }
+ //  //     helper(n - 1);
+ //  //   }
+ //  //  };
+ //  // helper(n);
+
+ //  // console.log(solution.rows())
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution.rows();
+  return solution;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solution = new Board({'n': n});
+  solutionCount = 0;
+  var helper = function(n, row) {
 
+    if (n - row === 1) {
+      solutionCount++;
+      return;
+    }
+
+    for (var i = 0; i < n; i++) {
+      solution.togglePiece(row, i);
+      if (solution.hasAnyQueensConflicts()) {
+        solution.togglePiece(row, i);
+      } else {
+        helper(n, row + 1);
+        solution.togglePiece(row, i);
+      }
+
+    }
+  };
+  helper(n, 0);
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
